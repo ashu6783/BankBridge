2666456b87c4058f005691149bbf2ec8e8a36b8a
ashutosh gaurav <ashu00665@gmail.com>
2025-03-29 23:44:46 +0530

    BB-1 updated mobile view for navigation

--- components/nav-button.tsx
+++ components/nav-button.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link"
+import { cn } from "@/lib/utils"
 import { Button } from "./ui/button"
+
 
 type Props = {
     href: string;
@@ -9,7 +11,7 @@ type Props = {
 
 export const NavButton = ({
     href,
-    label,
+    label, // Uncommented this line
     isActive,
 }: Props) => {
     return (
@@ -17,9 +19,13 @@ export const NavButton = ({
             asChild
             size="sm"
             variant='outline'
-            className={isActive ? `ss` : "ss"}
+            className={cn(
+                "w-full lg:w-auto justify-between font-sm font-semibold hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-none text-white focus:bg-white/30 transition",
+                isActive ? "bg-white/20 text-white" : "bg-transparent",
+            )}
         >
             <Link href={href}>
+                {label}
             </Link>
         </Button>
     )
